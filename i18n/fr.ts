48547d212d70ffb7e5ec11ce9660861620e408c5
lbartoletti <l.bartoletti@free.fr>
2019-07-05 08:57:57 +0200

    update translation

--- i18n/fr.ts
+++ i18n/fr.ts
@@ -2,66 +2,152 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="fr_FR" sourcelanguage="en">
 <context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../label_connector_dialog_settings.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dialogue</translation>
+    </message>
+    <message>
+        <location filename="../label_connector_dialog_settings.ui" line="20"/>
+        <source>Default style:</source>
+        <translation>Style par défaut :</translation>
+    </message>
+    <message>
+        <location filename="../label_connector_dialog_settings.ui" line="37"/>
+        <source>Always show connector style window</source>
+        <translation>Toujour afficher la fenêtre du style de connecteur</translation>
+    </message>
+</context>
+<context>
     <name>LabelConnector</name>
     <message>
-        <location filename="../label_connector.py" line="199"/>
+        <location filename="../label_connector.py" line="214"/>
         <source>&amp;Label Connector</source>
         <translation>Connecteur d&apos;étiquettes</translation>
     </message>
     <message>
-        <location filename="../label_connector.py" line="177"/>
+        <location filename="../label_connector.py" line="184"/>
         <source>Create label connector</source>
         <translation>Créer un connecteur d&apos;étiquette</translation>
     </message>
     <message>
-        <location filename="../label_connector.py" line="185"/>
+        <location filename="../label_connector.py" line="192"/>
         <source>Label connector settings</source>
         <translation>Préférences du connecteur d&apos;étiquettes</translation>
     </message>
     <message>
-        <location filename="../label_connector.py" line="227"/>
+        <location filename="../label_connector.py" line="244"/>
         <source>No active vector layer</source>
         <translation>Aucune couche vectorielle active</translation>
     </message>
     <message>
-        <location filename="../label_connector.py" line="227"/>
+        <location filename="../label_connector.py" line="244"/>
         <source>Label connector requires a vector layer</source>
         <translation>Le connecteur d&apos;étiquette requiert une couche vectorielle</translation>
     </message>
     <message>
-        <location filename="../label_connector.py" line="248"/>
+        <location filename="../label_connector.py" line="273"/>
         <source>No expression file</source>
         <translation>Aucun fichier d&apos;expression</translation>
     </message>
     <message>
-        <location filename="../label_connector.py" line="248"/>
+        <location filename="../label_connector.py" line="273"/>
         <source>Cannot find a previous expression file applied.</source>
         <translation>Impossible de trouver un fichier précédement appliqué.</translation>
     </message>
     <message>
-        <location filename="../label_connector.py" line="266"/>
+        <location filename="../label_connector.py" line="383"/>
         <source>Current style</source>
         <translation>Style courant</translation>
     </message>
     <message>
-        <location filename="../label_connector.py" line="266"/>
-        <source>Cannot change current style to &apos;label_style&apos;</source>
-        <translation>Impossible de changer le style courant en &apos;label_style&apos;</translation>
-    </message>
-    <message>
-        <location filename="../label_connector.py" line="270"/>
+        <location filename="../label_connector.py" line="387"/>
         <source>Add style</source>
         <translation>Ajouter un style</translation>
     </message>
     <message>
-        <location filename="../label_connector.py" line="270"/>
-        <source>Cannot add new style &apos;label_style&apos;</source>
-        <translation>Impossible d&apos;ajouter le nouveau style &apos;label_style&apos;</translation>
-    </message>
-    <message>
-        <location filename="../label_connector.py" line="278"/>
+        <location filename="../label_connector.py" line="395"/>
         <source>Error</source>
         <translation>Erreur</translation>
+    </message>
+    <message>
+        <location filename="../label_connector.py" line="201"/>
+        <source>Help</source>
+        <translation>Aide</translation>
+    </message>
+    <message>
+        <location filename="../label_connector.py" line="252"/>
+        <source>Unsupported symbol type</source>
+        <translation>Type de symbôle non supporté</translation>
+    </message>
+    <message>
+        <location filename="../label_connector.py" line="252"/>
+        <source>Cannot apply a label connector to the current symbol style.</source>
+        <translation>Impossible d&apos;appliquer un connecteur d&apos;étiquette sur le style de symbôle courant.</translation>
+    </message>
+    <message>
+        <location filename="../label_connector.py" line="285"/>
+        <source>Auxiliary Layer Error</source>
+        <translation>Erreur de stockage auxiliaire</translation>
+    </message>
+    <message>
+        <location filename="../label_connector.py" line="373"/>
+        <source>LabelConnector already exists</source>
+        <translation>LabelConnector existe déjà</translation>
+    </message>
+    <message>
+        <location filename="../label_connector.py" line="373"/>
+        <source>If you want to add a new LabelConnector on the style, please remove the comment in the expression first.</source>
+        <translation>Si vous voulez ajouter un nouveau LabelConnector sur le style, veuillez d&apos;abord décommenter l&apos;expression.</translation>
+    </message>
+    <message>
+        <location filename="../label_connector.py" line="380"/>
+        <source>Success</source>
+        <translation>Succès</translation>
+    </message>
+    <message>
+        <location filename="../label_connector.py" line="380"/>
+        <source>Label connector successfully created</source>
+        <translation>Connecteur d&apos;étiquette réalisé</translation>
+    </message>
+    <message>
+        <location filename="../label_connector.py" line="383"/>
+        <source>Cannot change current style to &apos;%&apos;</source>
+        <translation>Impossible de changer le style sur &apos;%&apos;</translation>
+    </message>
+    <message>
+        <location filename="../label_connector.py" line="387"/>
+        <source>Cannot add new style &apos;%&apos;</source>
+        <translation>Impossible d&apos;ajouter le nouveau style &apos;%&apos;</translation>
+    </message>
+</context>
+<context>
+    <name>LabelConnectorDialogBase</name>
+    <message>
+        <location filename="../label_connector_dialog_base.ui" line="14"/>
+        <source>Label Connector</source>
+        <translation>Connecteur d&apos;étiquette</translation>
+    </message>
+    <message>
+        <location filename="../label_connector_dialog_base.ui" line="20"/>
+        <source>Connector style</source>
+        <translation>Style de connecteur</translation>
+    </message>
+    <message>
+        <location filename="../label_connector_dialog_base.ui" line="27"/>
+        <source>Styles are located into the plugin directory &quot;labelStyles&quot;</source>
+        <translation>Les styles sont situés dans le dossier &apos;labelStyles&apos; du répertoire du plugin</translation>
+    </message>
+    <message>
+        <location filename="../label_connector_dialog_base.ui" line="44"/>
+        <source>Go to extension/LabelConnector to get this window back</source>
+        <translation>Veuillez vous rendre dans le menu extension/LabelConnector pour afficher cette fenêtre</translation>
+    </message>
+    <message>
+        <location filename="../label_connector_dialog_base.ui" line="47"/>
+        <source>Remember choice</source>
+        <translation>Se souvenir du choix</translation>
     </message>
 </context>
 </TS>
