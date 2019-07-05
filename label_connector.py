Oslandia/labelconnector

commit c49d74b5910b359b081aeba91854b3fd5ccff3ce

fix #15 use existing fieldName for simple label else use the first field name

label_connector.py
@@ -317,7 +317,12 @@ class LabelConnector:
 
         pal_layer = QgsPalLayerSettings()
         pal_layer.setDataDefinedProperties(pc)
-        pal_layer.fieldName = self.layer.fields()[0].name()
+        if self.layer.labeling().settings().fieldName == '':
+            pal_layer.fieldName = self.layer.fields()[0].name()
+            pal_layer.isExpression = False
+        else:
+            pal_layer.fieldName = self.layer.labeling().settings().fieldName
+            pal_layer.isExpression = self.layer.labeling().settings().isExpression
         if self.layer.geometryType() == QgsWkbTypes.LineGeometry:
             pal_layer.placement = QgsPalLayerSettings.Line
         pal_layer.enabled = True

commit 2e2a77526b3e0883b9c441994272233903b6961b

fix error introduced in c49d74b5910b359b081aeba91854b3fd5ccff3ce

label_connector.py
@@ -320,12 +320,13 @@ class LabelConnector:
 
         pal_layer = QgsPalLayerSettings()
         pal_layer.setDataDefinedProperties(pc)
-        if self.layer.labeling().settings().fieldName == '':
+        labeling = self.layer.labeling()
+        if labeling is None:
             pal_layer.fieldName = self.layer.fields()[0].name()
             pal_layer.isExpression = False
         else:
-            pal_layer.fieldName = self.layer.labeling().settings().fieldName
-            pal_layer.isExpression = self.layer.labeling().settings().isExpression
+            pal_layer.fieldName = labeling.settings().fieldName
+            pal_layer.isExpression = labeling.settings().isExpression
         if self.layer.geometryType() == QgsWkbTypes.LineGeometry:
             pal_layer.placement = QgsPalLayerSettings.Line
         pal_layer.enabled = True